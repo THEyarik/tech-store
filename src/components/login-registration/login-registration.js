6c16d4718daf00b619b3558bc0d3d632163ca426
PashkaSm <smolynets_pavlo@ukr.net>
2023-05-13 22:33:41 +0300

    reg add

--- src/components/login-registration/login-registration.js
+++ src/components/login-registration/login-registration.js
@@ -5,8 +5,24 @@ import "./login-registration.css";
 
 
 function LoginRegistration() {
-    const [dateNewClient, setDateNewClient] = useState();
+    const [dateNewClient, setDateNewClient] = useState()
+    const [loginDate, setLoginDate] = useState()
 
+    const loginClient = async () => {
+
+        const res = await Axios.post("http://localhost:39510/authentication/login", loginDate).then(
+            (res) => {
+                setListClient(res.data)
+                console.log("Posting data :", res);
+                console.log("well done")
+            }
+        )
+
+    }
+    const loginChange = (e) => {
+        setLoginDate(prev => ({ ...prev, [e.target.name]: e.target.value }))
+        console.log(loginDate)
+    }
     const handleChange = (e) => {
         setDateNewClient(prev => ({ ...prev, [e.target.name]: e.target.value }))
         console.log(dateNewClient)
@@ -21,15 +37,12 @@ function LoginRegistration() {
         await Axios.post("http://localhost:39510/clients/register", dateNewClient)
             .then((res) => {
                 console.log("Posting data :", res);
-                console.log("well done")
-                // eslint-disable-next-line no-restricted-globals
-                //history.push("/*");
+                
             })
             .catch((err) => console.log(err));
 
         // navigate("/")
     }
-
 
     const loginHandlerButton = (e) => {
         const signupBtn = document.getElementById('signup');
@@ -43,14 +56,13 @@ function LoginRegistration() {
             }
         });
     }
-
-
     const signupHandlerButton = (e) => {
         const loginBtn = document.getElementById('login');
         let parent = e.target.parentNode;
         Array.from(e.target.parentNode.classList).find((element) => {
             if (element !== "slide-up") {
                 parent.classList.add('slide-up')
+
             } else {
                 loginBtn.parentNode.parentNode.classList.add('slide-up')
                 parent.classList.remove('slide-up')
@@ -63,7 +75,7 @@ function LoginRegistration() {
             <div className="login__registration">
                 <div className="container login__registration__container ">
                     <div className="form-structor">
-                        <form >
+                        <form action='#'>
                             <div className="signup" onClick={signupHandlerButton}>
                                 <h2 className="form-title" id="signup"><span>or</span>Sign up</h2>
                                 <div className="form-holder">
@@ -77,17 +89,17 @@ function LoginRegistration() {
                             </div>
                         </form>
 
-                        <form >
+                        <form action='#'>
                             <div className="login slide-up" onClick={loginHandlerButton}>
                                 <div className="center">
                                     <h2 className="form-title" id="login"><span>or</span>Log in</h2>
                                     <div className="form-holder">
                                         <input type="email" className="input" placeholder="Email"
-                                            name="user_email" />
+                                            name="username" onChange={loginChange} />
                                         <input type="password" className="input" placeholder="Password"
-                                            name="user_password" />
+                                            name="password" onChange={loginChange} />
                                     </div>
-                                    <button className="submit-btn">Log in</button>
+                                    <button className="submit-btn" onClick={loginClient}>Log in</button>
                                 </div>
                             </div>
                         </form>
